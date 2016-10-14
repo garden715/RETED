81a5630c39fe68bf43a83e453b4d135d36717d46
seojungwon <garden715@gmail.com>
2016-10-14 20:16:37 +0900

    add second viewController

--- reted/ViewController.swift
+++ reted/ViewController.swift
@@ -7,19 +7,101 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        if let user = FIRAuth.auth()?.currentUser {
+            self.signedIn(user)
+        }
     }
+    
+    @IBAction func didTapSignIn(_ sender: AnyObject) {
+        // Sign In with credentials.
+        guard let email = emailField.text, let password = passwordField.text else {
+            return }
+        FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                self.alertMessage(_message: error.localizedDescription)
+                return
+            }
+            self.signedIn(user!)
+        }
+    }
+    @IBAction func didTapSignUp(_ sender: AnyObject) {
+        guard let email = emailField.text, let password = passwordField.text else { return }
+        FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                self.alertMessage(_message: error.localizedDescription)
+                return
+            }
+            self.setDisplayName(user!)
+        }
+    }
+    
+    func setDisplayName(_ user: FIRUser) {
+        let changeRequest = user.profileChangeRequest()
+        changeRequest.displayName = user.email!.components(separatedBy: "@")[0]
+        changeRequest.commitChanges(){ (error) in
+            if let error = error {
+                self.alertMessage(_message: error.localizedDescription)
+                return
+            }
+            self.signedIn(FIRAuth.auth()?.currentUser)
+        }
+    }
+    
+    @IBAction func didRequestPasswordReset(_ sender: AnyObject) {
+        let prompt = UIAlertController.init(title: nil, message: "Email:", preferredStyle: .alert)
+        let okAction = UIAlertAction.init(title: "OK", style: .default) { (action) in
+            let userInput = prompt.textFields![0].text
+            if (userInput!.isEmpty) {
+                return
+            }
+            FIRAuth.auth()?.sendPasswordReset(withEmail: userInput!) { (error) in
+                if let error = error {
+                    self.alertMessage(_message: error.localizedDescription)
+                    return
+                }
+            }
+        }
+        prompt.addTextField(configurationHandler: nil)
+        prompt.addAction(okAction)
+        present(prompt, animated: true, completion: nil);
+    }
+    
+    func signedIn(_ user: FIRUser?) {
+        MeasurementHelper.sendLoginEvent()
+        
+        AppState.sharedInstance.displayName = user?.displayName ?? user?.email
+        AppState.sharedInstance.photoURL = user?.photoURL
+        AppState.sharedInstance.signedIn = true
+        let notificationName = Notification.Name(rawValue: Constants.NotificationKeys.SignedIn)
+        NotificationCenter.default.post(name: notificationName, object: nil, userInfo: nil)
+        performSegue(withIdentifier: Constants.Segues.SignInToFp, sender: nil)
+    }
+    
+    func alertMessage(_message:String) {
+        
+        let alert = UIAlertController(title: "Alert", message: _message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Confirm", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
 
+    }
 
 }
 
